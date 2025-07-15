0dd81b026d14623d3925ab9447bc96fe439f2c4c
nhtnhanbn <nhtnhanbn@gmail.com>
2025-07-15 14:01:26 +1000

    Add map

--- site/src/index.js
+++ site/src/index.js
@@ -1,10 +1,27 @@
-const map = document.querySelector("#map");
-map.textContent = "Hello";
+import "./style.css";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
-fetch("http://10.0.0.238:3000")
-.then((response) => {
+let feed;
+fetch(
+    "http://10.0.0.238:3000"
+).then((response) => {
     return response.json();
-})
-.then((feed) => {
-    map.textContent = JSON.stringify(feed);
+}).then((response) => {
+    feed = response;
 });
+
+const map = L.map("map").setView([-37.8, 145], 11);
+
+L.tileLayer(
+    "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+        maxZoom: 19,
+        attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+    }
+).addTo(map);
+
+L.circle(
+    [-37.8, 145],
+    { radius: 100 }
+).addTo(map);
