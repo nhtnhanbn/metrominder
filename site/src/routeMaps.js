80266824c9d590fd5994115fb12d3452e925d574
nhtnhanbn <nhtnhanbn@gmail.com>
2025-11-23 16:56:40 +1100

    Add regional train initialisation of route structures

--- site/src/routeMaps.js
+++ site/src/routeMaps.js
@@ -1,10 +1,11 @@
-import { metroTrainRouteMaps } from "./routeMapsInitial.js";
+import { metroTrainRouteMaps, regionTrainRouteMaps } from "./routeMapsInitial.js";
 import metroTrainRouteData from "../../data/gtfsschedule/2/routes.txt";
 import metroTramRouteData from "../../data/gtfsschedule/3/routes.txt";
-import metroTrainStopRoutes from "../../data/metroTrainStopRoutes.json";
-import metroTramStopRoutes from "../../data/metroTramStopRoutes.json";
+import regionTrainRouteData from "../../data/gtfsschedule/1/routes.txt";
+import stopRoutes from "../../data/stopRoutes.json";
 import metroTrainGeojson from "./metroTrainRoutes.geojson";
 import metroTramGeojson from "./metroTramRoutes.geojson";
+import regionTrainGeojson from "./regionTrainRoutes.geojson";
 
 class MetroTramRouteMap {
     constructor(routeCode) {
@@ -15,24 +16,28 @@ class MetroTramRouteMap {
     }
 }
 
-function createRouteStructures(mode) {
-    let routeMaps, routeData;
-    if (mode === "metroTrain") {
-        routeMaps = metroTrainRouteMaps;
-        routeData = metroTrainRouteData;
-    } else if (mode === "metroTram") {
-        routeMaps = new Set();
-        routeData = metroTramRouteData;
+function createRouteStructures(modes) {
+    let routeData = [], routeMaps = new Set();
+    for (const mode of modes) {
+        if (mode === "metroTrain") {
+            routeData.push(...metroTrainRouteData);
+            routeMaps = routeMaps.union(metroTrainRouteMaps);
+        } else if (mode === "metroTram") {
+            routeData.push(...metroTramRouteData);
 
-        for (const routeDatum of routeData.sort((a, b) => {
-            return a.route_short_name - b.route_short_name;
-        })) {
-            const routeCode = routeDatum.route_short_name;
-            routeMaps.add(new MetroTramRouteMap(routeCode));
+            for (const routeDatum of routeData.sort((a, b) => {
+                return a.route_short_name - b.route_short_name;
+            })) {
+                const routeCode = routeDatum.route_short_name;
+                routeMaps.add(new MetroTramRouteMap(routeCode));
+            }
+        } else if (mode === "regionTrain") {
+            routeData.push(...regionTrainRouteData);
+            routeMaps = routeMaps.union(regionTrainRouteMaps);
         }
     }
 
-    const routeById = {}, routeByShortName = {};
+    const routeById = {}, routeByCode = {};
 
     for (const routeMap of routeMaps) {
         routeById[routeMap.routeId] = routeMap;
@@ -45,39 +50,31 @@ function createRouteStructures(mode) {
             routeMap.routeLongName = routeDatum.route_long_name;
             routeMap.routeColour = "#" + routeDatum.route_color;
             routeMap.routeTextColour = "#" + routeDatum.route_text_color;
+            routeMap.stopIds = stopRoutes[routeMap.routeId];
         }
     }
 
     for (const routeMap of routeMaps) {
-        routeByShortName[routeMap.routeShortName] = routeMap;
+        routeByCode[routeMap.routeCode] = routeMap;
     }
 
-    if (mode === "metroTrain") {
-        for (const routeDatum of routeData) {
-            if (routeDatum.route_id in routeById) {
-                const routeMap = routeById[routeDatum.route_id];
-                routeMap.stopIds = metroTrainStopRoutes[routeMap.routeId];
+    for (const mode of modes) {
+        if (mode === "metroTrain") {
+            for (const feature of metroTrainGeojson.features) {
+                routeByCode[feature.properties.SHAPE_ID.slice(2, 5)].geojson.push(feature);
             }
-        }
-        for (const feature of metroTrainGeojson.features) {
-            const routeMap = routeByShortName[feature.properties.SHORT_NAME];
-            if (routeMap.shapeIds.includes(feature.properties.SHAPE_ID)) {
-                routeMap.geojson.push(feature);
+        } else if (mode === "metroTram") {
+            for (const feature of metroTramGeojson.features) {
+                routeByCode[feature.properties.SHORT_NAME].geojson.push(feature);
             }
-        }
-    } else if (mode === "metroTram") {
-        for (const routeDatum of routeData) {
-            if (routeDatum.route_id in routeById) {
-                const routeMap = routeById[routeDatum.route_id];
-                routeMap.stopIds = metroTramStopRoutes[routeMap.routeId];
+        } else if (mode === "regionTrain") {
+            for (const feature of regionTrainGeojson.features) {
+                routeByCode[feature.properties.SHAPE_ID.slice(2, 5)].geojson.push(feature);
             }
-        }
-        for (const feature of metroTramGeojson.features) {
-            routeByShortName[feature.properties.SHORT_NAME].geojson.push(feature);
         }
     }
 
-    return { routeMaps, routeById, routeByShortName };
+    return { routeMaps, routeById, routeByCode };
 }
 
 export { createRouteStructures };
