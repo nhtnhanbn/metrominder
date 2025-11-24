cf6170e75aaa0f008cdbdb519559be8f75476417
nhtnhanbn <nhtnhanbn@gmail.com>
2025-11-24 14:55:42 +1100

    Identify and consolidate bus routes by route number code

--- data/getStopRoutes.mjs
+++ data/getStopRoutes.mjs
@@ -9,26 +9,30 @@ import { parse } from "csv-parse/sync";
         const trips = parse(rawTrips, { bom: true, columns: true });
         const tripRoute = {};
         for (const trip of trips) {
-            tripRoute[trip.trip_id] = trip.route_id;
+            tripRoute[trip.trip_id] = number === 4 ? trip.route_id.slice(3, 6) : trip.route_id;
         }
         console.log(`Processed trips for ${number}.`)
 
         const rawRoutes = await fs.readFile(`./gtfsschedule/${number}/routes.txt`);
         const routes = parse(rawRoutes, { bom: true, columns: true });
         for (const route of routes) {
-            stopRoutes[route.route_id] = new Set();
+            stopRoutes[number === 4 ? route.route_short_name : route.route_id] = new Set();
         }
         console.log(`Processed routes for ${number}.`)
         
         const rawStopTimes = await fs.readFile(`./gtfsschedule/${number}/stop_times.txt`);
         const stopTimes = parse(rawStopTimes, { bom: true, columns: true });
         for (const stopTime of stopTimes) {
-            stopRoutes[tripRoute[stopTime.trip_id]].add(stopTime.stop_id)
+            const routeId = tripRoute[stopTime.trip_id];
+            if (routeId in stopRoutes) {
+                stopRoutes[routeId].add(stopTime.stop_id);
+            }
         }
         console.log(`Processed stop times for ${number}.`)
 
         for (const route of routes) {
-            stopRoutes[route.route_id] = Array.from(stopRoutes[route.route_id]);
+            const routeId = number === 4 ? route.route_short_name : route.route_id;
+            stopRoutes[routeId] = Array.from(stopRoutes[routeId]);
         }
         console.log(`Arrayified for ${number}.`)
     }
