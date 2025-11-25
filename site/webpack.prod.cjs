4747e79bbed991c6387be21d5141646bfa348b9d
nhtnhanbn <nhtnhanbn@gmail.com>
2025-11-26 01:28:40 +1100

    Fix webpack production configuration import

--- site/webpack.prod.cjs
+++ site/webpack.prod.cjs
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.cjs");
+const webpack = require("webpack");
 
 module.exports = merge(common, {
     mode: "production",
